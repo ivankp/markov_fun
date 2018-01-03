dc31b9d002325207b435cd18882d4a2bb98f104c
ivankp <ivan.pogrebnyak@gmail.com>
2018-01-02 19:44:56 -0500

    lowercase version

--- src/markov.cc
+++ src/markov.cc
@@ -10,11 +10,11 @@
 using std::cout;
 using std::wcout;
 using std::endl;
+using std::get;
+using namespace std::string_literals;
 
 template <typename F, size_t... I>
 void for_(F f, std::index_sequence<I...>) {
-  // using expander = int[];
-  // (void)expander{0, ((void)f(std::integral_constant<size_t,I>{}), 0)...};
   ( f(std::integral_constant<size_t,I>{}),... );
 }
 template <size_t N, typename F>
@@ -31,7 +31,7 @@ struct tuple_hash {
     size_t seed = 0;
     for_<std::tuple_size<T>::value>(
       [&](auto i){
-        hash_combine(seed,std::get<i.value>(t));
+        hash_combine(seed,get<i.value>(t));
       }
     );
     return seed;
@@ -39,7 +39,7 @@ struct tuple_hash {
 };
 
 struct word_properties {
-  std::set<wchar_t> punct;
+  // std::set<wchar_t> punct;
   bool cap = true;
 };
 
@@ -51,11 +51,46 @@ public:
   template <typename T>
   const auto& operator()(const T& container) {
     auto it = container.begin();
-    std::uniform_int_distribution<unsigned int> dist(0,container.size());
+    std::uniform_int_distribution<unsigned int> dist(0,container.size()-1);
     std::advance(it,dist(gen));
     return *it;
   }
 } select_rand;
+
+std::set<std::wstring> abbrev {{
+  L"mr"s, L"mrs"s, L"e.g"s
+}};
+
+std::unordered_map<std::wstring,word_properties> dict;
+struct dict_ptr {
+  using type = decltype(dict)::value_type*;
+  type value;
+  dict_ptr& operator=(type ptr) noexcept {
+    value = ptr;
+    return *this;
+  }
+  auto operator* () const noexcept { return value->first; }
+  type operator->() const noexcept { return value; }
+  operator bool() const noexcept { return value; }
+};
+namespace std {
+template <> struct hash<dict_ptr> {
+  size_t operator()(const dict_ptr& t) const {
+    return std::hash<dict_ptr::type>{}(t.value);
+  }
+};
+}
+std::basic_ostream<wchar_t>& operator<<(
+  std::basic_ostream<wchar_t>& wout, const dict_ptr& ptr
+) {
+  return wout << *ptr;
+}
+std::array<dict_ptr,2> key;
+std::unordered_map<
+  decltype(key),
+  std::set<dict_ptr>,
+  tuple_hash
+> map;
 
 int main(int argc, char* argv[]) {
   if (argc==1) {
@@ -64,14 +99,7 @@ int main(int argc, char* argv[]) {
   }
 
   std::wstring word;
-  std::unordered_map<std::wstring,word_properties> dict;
-  std::array<std::wstring,2> key;
-  std::unordered_map<
-    decltype(key),
-    const decltype(dict)::value_type*,
-    tuple_hash
-  > map;
-
+  dict_ptr dict_word;
   std::locale::global(std::locale(""));
   bool cap = true;
   for (int i=1; i<argc; ++i) {
@@ -79,32 +107,30 @@ int main(int argc, char* argv[]) {
 
     for (wchar_t c; f.get(c); ) {
       if (std::isspace(c)) {
-        if (!std::get<0>(key).empty() &&
-            !std::get<1>(key).empty() &&
-            !word.empty()
-        ) {
-          size_t a = 0, b = word.size();
-          for (; b; ) if (!std::ispunct(word[--b])) break;
-          for (; a<=b; ++a) if (!std::ispunct(word[a])) break;
-          ++b;
-          if (a!=b) {
-            auto word_no_punct = word.substr(a,b-a);
-            auto& dict_word = *dict.try_emplace(word_no_punct).first;
-            for (size_t i=b; i<word.size(); ++i)
-              dict_word.second.punct.insert(word[i]);
-            word = std::move(word_no_punct);
-            map[key] = &dict_word;
-            if (!cap) dict_word.second.cap = false;
-          } else {
-            word.clear();
+
+        if (word.empty()) continue;
+        const auto last = word.back();
+        size_t a = 0, b = word.size();
+        for (; b; ) if (!std::ispunct(word[--b])) break;
+        for (; a<=b; ++a) if (!std::ispunct(word[a])) break;
+        ++b;
+        word = word.substr(a,b-a);
+        if (word.empty()) continue;
+        dict_word = &*dict.try_emplace(word).first;
+
+        if (get<0>(key) && get<1>(key)) {
+          map[key].insert(dict_word);
+          if (!cap) dict_word->second.cap = false;
+          if (last=='.' && !abbrev.count(word)) {
+            get<0>(key) = nullptr;
+            get<1>(key) = nullptr;
           }
         }
-        if (!word.empty()) {
-          std::get<0>(key) = std::get<1>(key);
-          std::get<1>(key) = word;
-          word.clear();
-        }
+        get<0>(key) = get<1>(key);
+        get<1>(key) = dict_word;
+        word.clear();
         cap = true;
+
       } else {
         const auto lc = std::tolower(c);
         if (word.empty() && lc==c) cap = false;
@@ -118,26 +144,25 @@ int main(int argc, char* argv[]) {
 
   /*
   for (const auto& x : map) {
-    wcout << "(" << x.first[0] << ',' << x.first[1] << ") = "
-      << x.second->first << ' ' << x.second->second.cap << ' ';
-    for (auto c : x.second->second.punct)
-      wcout << c;
+    wcout << "(" << x.first[0] << ',' << x.first[1] << ") =";
+    for (const auto& w : x.second)
+      wcout <<' '<< w;
     wcout << endl;
   }
   */
 
   auto& seed = select_rand(map);
-  key  = seed.first;
-  word = seed.second->first;
+  key = seed.first;
+  dict_word = select_rand(seed.second);
 
-  wcout << key[0] <<' '<< key[1] <<' '<< word;
+  wcout << key[0] <<' '<< key[1] <<' '<< dict_word;
   for (unsigned i=0; i<35; ++i) {
-    std::get<0>(key) = std::get<1>(key);
-    std::get<1>(key) = word;
+    get<0>(key) = get<1>(key);
+    get<1>(key) = dict_word;
     const auto it = map.find(key);
     if (it==map.end()) break;
-    word = it->second->first;
-    wcout <<' '<< word;
+    dict_word = select_rand(it->second);
+    wcout <<' '<< dict_word;
   }
   wcout << endl;
 }
